c6f0d3828a25d96ea62bcc0f92de052705b2cd4f
sylviaklaipedos <130221505+sylviaklaipedos@users.noreply.github.com>
2023-12-02 15:23:41 +0500

    let n = 3; let array = [2, 1, 3, 8] let index = array.indexOf(n);
    if (index === -1) console.log('элемент не найден');
    else console.log('индекс элемента = ' + index);

--- thirdPractice/thirdTask.js
+++ thirdPractice/thirdTask.js
@@ -1,13 +1,6 @@
-let n = 7;
-let array = [2, 1, 3]
-let found = false;
+let n = 3;
+let array = [2, 1, 3, 8]
+let index = array.indexOf(n);
 
-for (let i = 0; i < array.length; ++i) {
-    if (array[i] === n) {
-        found = true;
-        console.log('индекс элемента = ' + i);
-        break;
-    }    
-}
-
-if (!found) console.log('элемент не найден');
+if (index === -1) console.log('элемент не найден');
+else console.log('индекс элемента = ' + index);
